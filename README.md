b078ddc4808af11c5648dc03201923e4fc036682
Dennis Dang <dangggdennis@gmail.com>
2017-07-19 11:47:37 -0700

    version 3.0 with Node/mySQL server

--- README.md
+++ README.md
@@ -1,36 +1,7 @@
-# SGT - Student Grade Table
+### Instructions
 
-## Version 0.1
-#### Description
-Version 0.1 is the starting point of the Student Grade Table Project. LearningFuze has provided a base structure that
-includes HTML structure that will need to be updated with boostrap classes. For this version we are looking to see that you
-understand how to style an application with bootstrap, take input values and apply them to objects, and update the DOM
-with a visual of those objects stored.
+- use the dataDump.sql file and import it via phpmyadmin SQL tab.  copy and paste the SQL into there to recreate the DB
+- copy the mysql_connect.php.config file to mysql_connect.php and put in the appropriate data for your local server.  probably user: root and password: root
+- follow the instructions inside data.php (which will also include altering the files in the dataApi directory)
 
-- <a href="https://github.com/Learning-Fuze/SGT/tree/v.1lf#getting-started">Getting Started</a>
-- <a href="https://github.com/Learning-Fuze/SGT/tree/v.1lf#scope">View Scope</a>
-- <a href="https://github.com/Learning-Fuze/SGT/tree/v.1lf#design">View Design</a>
-- <a href="https://github.com/Learning-Fuze/SGT/tree/v.1lf#assignments---aka-criteria-for-success-on-this-version-of-the-project">View Assignments</a>
-
-## Version 0.5
-#### Description
-Version 0.5 starts where v.1 left off with adding in the delete functionality. When clicking on the delete button we need
-to add an event handler that will remove the current row from the DOM only after we have successfully removed the object
-from the array.
-
-- <a href="https://github.com/Learning-Fuze/SGT/tree/v.5lf#getting-started">Getting Started</a>
-- <a href="https://github.com/Learning-Fuze/SGT/tree/v.5lf#scope">View Scope</a>
-
-## Version 1.0
-#### Description
-Version 1.0 picks up where 0.5 left off. You will now be populating records from a database via the LearningFuze SGT API. This version will only be pulling entries, you will not be adding anything to the database for this version. You should still be able to add entries locally as you did before.
-
-- <a href="https://github.com/Learning-Fuze/SGT/tree/v1.0lf#getting-started" target="_blank">Getting Started</a>
-- <a href="https://github.com/Learning-Fuze/SGT/tree/v1.0lf#scope" target="_blank">View Scope</a>
-
-## Version 2.0
-#### Description
-Version 2.0 adds more CRUD functionality.  Previously you added R (Read), and now you will be adding Create and Delete functionality. You will now be adding records to a database via the LearningFuze SGT API.
-
-- <a href="https://github.com/Learning-Fuze/SGT/tree/v2.0lf#getting-started" target="_blank">Getting Started</a>
-- <a href="https://github.com/Learning-Fuze/SGT/tree/v2.0lf#scope" target="_blank">View Scope</a>
+- alter the ajax calls in your SGT to point to data.php, and give it a query string parameter of "action" with a value equal to the values you are checking in the switch in data.php
--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function SGT() {
 	self.init = function() {
 		$(document).ready(function() {
 			self.eventHandlers();
-		})
-	}
+		});
+	};
 
 	/**
 	* Event handlers
@@ -30,8 +30,8 @@ function SGT() {
 		})
 		$("#getDataKey").on("click", function() {
 			self.getStudentData();
-		})
-	}
+		});
+	};
 
 	/**
 	 * Define all global variables here
@@ -43,19 +43,16 @@ function SGT() {
 	 */ 
 	 var student_array = [
 	 	{	name: "Dennis Dang",
-	 		course: "Bio199",
-	 		grade: "96",
-	 		id: 999
+	 		course_name: "Artificial Intelligence I",
+	 		grade: "96"
 	 	},
 	 	{	name: "Megumi Iwama",
-	 		course: "Dance101",
-	 		grade: "100",
-	 		id: 1000
+	 		course_name: "Contemporary 301",
+	 		grade: "100"
 	 	},
 	 	{	name: "Derek Smith",
-	 		course: "KickAss",
-	 		grade: "59",
-	 		id: 1001
+	 		course_name: "Metallurgy",
+	 		grade: "59"
 	 	}
  	];
 
@@ -63,60 +60,50 @@ function SGT() {
 	* inputIds - id's of the elements that are used to add students
 	* @type {string[]}
 	*/	
-	var inputIds = ["#studentName","#course","#studentGrade","#operation"];
-
-	self.Student = function(studentObj) {
-		self.name = studentObj.name;
-		self.course = studentObj.course;
-		self.grade = studentObj.name;
-		self.delete = function() {
-
-		}
-	}
+	var inputIds = ["#studentName","#course_name","#studentGrade","#operation"];
 
 	/**
 	 * addStudent - creates a student objects based on input fields in the 
 	 * form and adds the object to global student array
 	 * @return undefined
 	 */	
-		self.addStudent = function(){
-		 	var studentName = $(inputIds[0]).val();
-		 	var studentCourse = $(inputIds[1]).val();
-		 	var studentGrade = $(inputIds[2]).val();
-		 	var studentObj = {
-		 		name: studentName,
-		 		course: studentCourse,
-		 		grade: studentGrade
-
-		 	};
-		 	student_array.push(studentObj);
-		 	return studentObj;
-		 };
+	self.addStudent = function(){
+	 	var name = $(inputIds[0]).val();
+	 	var course_name = $(inputIds[1]).val();
+	 	var grade = $(inputIds[2]).val();
+	 	var studentObj = {
+	 		name,
+	 		course_name,
+	 		grade
+	 	};
+	 	student_array.push(studentObj);
+	 	return studentObj;
+	 };
 
 	/**
 	 * addClicked - Event Handler when user clicks the add button
 	 */
-		self.addClicked = function(){
-	 	var student = new Student(self.addStudent());
-	 	addStudentData(student);
-	 	updateData();
-	 	clearAddStudentForm();
+	self.addClicked = function(){
+	 	var student = self.addStudent();
+	 	self.addStudentData(student);
+	 	self.updateData();
+	 	self.clearAddStudentForm();
 	 	$("#studentName").focus();
-	 }
+	 };
 
 	 /**
 	 * cancelClicked - Event Handler when user clicks the cancel button, should clear out student form
 	 */
-		self.cancelClicked = function() {
+	self.cancelClicked = function() {
 	 	clearAddStudentForm();
-	 }
+	 };
 
 	 /**
 	 * clearAddStudentForm - clears out the form values based on inputIds variable
 	 */
-		self.clearAddStudentForm = function() {
+	self.clearAddStudentForm = function() {
 	 	$("input").val('');
-	 }
+	 };
 
 	// Adds event listener for enter key - adds student. 
 	// Reduces mouse movement to and fro adding student data
@@ -125,9 +112,9 @@ function SGT() {
 			var key = e.which || e.keyCode;
 			if(key === 13){
 				self.addClicked()
-			}
-		})
-	 }
+			};
+		});
+	 };
 
 	/**
 	 * calculateAverage - loop through the global student array and calculate average grade and return that value
@@ -140,78 +127,70 @@ function SGT() {
 	 	});
 	 	average = average / student_array.length;
 	 	return average.toFixed(2);
-	 }
+	 };
 
 	/**
 	 * updateData - centralized function to update the average and call student list update
 	 */
-		self.updateData = function() {
+	self.updateData = function() {
 	 	average = self.calculateAverage(student_array);
 	 	if(isNaN(average)){
 	 		average = 0;
-	 	}
+	 	};
 	 	$(".avgGrade").text(average);
 	 	self.updateStudentList();
-	 }	 
+	 }; 
 
 	self.getStudentData = function() {
+		student_array = [];
 		$.ajax({
-			method: "POST",
+			method: "GET",
 			dataType: "json",
-			url: "http://s-apis.learningfuze.com/sgt/get",
-			data: {api_key: "Pr0R1gXBEt"},
+			url: "http://localhost:8000/get",
 			success: function(response){
-				if(response.success){
-					response.data.forEach(function(el){
+				if(response){
+					console.log(response);
+					response.forEach(function(el){
 						student_array.push(el);
-					})
-				} else {
-					$("#ajaxMessage").text(response.error);	
-					$("#ajaxModal").modal()
-				}
+					});
+				};
 				self.updateData();
 				console.log("Student data retrieved: success");
 			},
 			error: function(err){
-				console.log("Student data retrieved: error")
+				console.log("Student data retrieved: error");
 			}
 		});
-	}
+	};
 
-	self.addStudentData = function(studentObj){
-		studentObj.api_key = "Pr0R1gXBEt";
+	self.addStudentData = function(student){
 		$.ajax({
 			method: "POST",
 			dataType: "json",
-			url: "http://s-apis.learningfuze.com/sgt/create",
-			data: studentObj,
+			url: "http://localhost:8000/create",
+			data: {
+				name: student.name,
+				course_name: student.course_name,
+				grade: student.grade
+			},
 			success: function(response){
-				if(response.error === true){
-					$("#ajaxMessage").text(response.error);	
-					$("#ajaxModal").modal()
-				}
-				console.log("Student data post: success")
+				console.log("Student data post: success");
 			},
 			error: function(err){
-				console.log("Student data post: error")
+				console.log("Student data post: error" , err)
 			}
-		})
-	}
+		});
+	};
 
 	self.deleteStudentData = function(studentObj){
 		$.ajax({
 			method: "POST",
 			dataType: "json",
-			url: "http://s-apis.learningfuze.com/sgt/delete",
+			url: "http://localhost:8000/delete",
 			data: {
-				api_key: "Pr0R1gXBEt",
-				student_id: studentObj.id
+				id: studentObj.id
 			},
 			success: function(response){
-				if(response.error === true){
-					$("#ajaxMessage").text(response.error);	
-					$("#ajaxModal").modal()
-				}
 				console.log("Student data delete: success")
 				var index = student_array.indexOf(studentObj);
 				student_array.splice(index,1);
@@ -219,8 +198,8 @@ function SGT() {
 			error: function(err){
 				console.log("Student data delete: error")
 			}
-		})
-	}
+		});
+	};
 
 	/**
 	 * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
@@ -239,7 +218,7 @@ function SGT() {
 	self.addStudentToDom = function(studentObj) {
 		var tableRow = $("<tr>").addClass("row")
 	 		var tName = $("<td>").text(studentObj.name);
-	 		var tCourse = $("<td>").text(studentObj.course);
+	 		var tCourse = $("<td>").text(studentObj.course_name);
 	 		var tGrade = $("<td>").text(studentObj.grade);
 	 		var tDelete = $("<td>");
 	 		var tButton = $("<button>",{
@@ -260,26 +239,26 @@ function SGT() {
 	/**
 	 * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
 	 */
-		self.reset = function(){
+	self.reset = function(){
 	 	var student_array = [
-	 	{	name: "Dennis Dang",
-	 		course: "Bio199",
-	 		grade: "96"
-	 	},
-	 	{	name: "Megumi Iwama",
-	 		course: "Dance101",
-	 		grade: "100"
-	 	},
-	 	{	name: "Derek Smith",
-	 		course: "KickAss",
-	 		grade: "59"
-	 	}
+		 	{	name: "Dennis Dang",
+		 		course_name: "Artificial Intelligence I",
+		 		grade: "96"
+		 	},
+		 	{	name: "Megumi Iwama",
+		 		course_name: "Contemporary 301",
+		 		grade: "100"
+		 	},
+		 	{	name: "Derek Smith",
+		 		course_name: "Metallurgy",
+		 		grade: "59"
+		 	}
 		];
-	 	self.updateData();
-	 }
+ 	self.updateData();
+	 };
 
 	self.init();
-}
+};
 
 //sort by fields
 //form validation
